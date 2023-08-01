b23b3b7234625f20717f4cb95a8c37bc0b9afd05
GSG-proj <mariamadnan879@gmail.com>
2023-08-01 12:08:51 +0300

    Task 2

---
+++ Task 2.py
@@ -0,0 +1,7 @@
+name=input("Enter your FullName")
+mobile=input("Enter your mobile number")
+year=int(input("Input your year of birthday"))
+age=2023-year
+print(f"your name is{name}")
+print(mobile.split("-"))
+print(f"Your current age is{age}")
