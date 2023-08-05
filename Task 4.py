d2755b996fa2346d8d64e0bee150d7d491b096ff
GSG-proj <mariamadnan879@gmail.com>
2023-08-05 11:03:02 +0300

    Task 5

--- Task 4.py
+++ Task 4.py
@@ -1,3 +1,11 @@
+def test(area):
+ if area>=10:
+     print("The Area is huge")
+ elif area<10:
+     print("The Area is small")
+ else :
+     print("invalid input")
+
 def Tringle():
   b=float(input("Enter base"))
   h=float(input("Enter Highe"))
@@ -5,14 +13,25 @@ def Tringle():
   area = b * h / 2
   per = b + h + x
   print(f"The Area is{area} \nThe perimeter is{per} ")
+  test(area)
 
-def square():
+def rectangle():
  b= float(input("Enter base"))
- area=b*b
- per=4*b
+ h = float(input("Enter Highe"))
+ area=b*h
+ per=(2*b)+(2*h)
  print(f"The Area is{area} \nThe perimeter is{per} ")
+ test(area)
+def circle():
+  radice=float(input("Enter the raduice"))
+  area=3.14*radice*radice
+  per=2*3.14*radice
+  print(f"The Area is{area} \nThe perimeter is{per} ")
+  test(area)
 
 print("To calculate the area & perimeter of triangle")
 Tringle()
-print("To calculate the area & perimeter of square")
-square()
+print("To calculate the area & perimeter of rectangle")
+rectangle()
+print("To calculate the area & perimeter of circle")
+circle()
