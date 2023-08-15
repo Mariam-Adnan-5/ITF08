5289dbda371ef4faa783dbd46e8e62259218e667
GSG-proj <mariamadnan879@gmail.com>
2023-08-15 15:15:02 +0300

    Task 7

---
+++ Task 7.py
@@ -0,0 +1,27 @@
+list=[]
+avarge=[]
+grads=[]
+minimum=[]
+maximum=[]
+students_numbers=int(input("Enter the number of students"))
+for i in range(students_numbers):
+    x=int(input(f"Enter the number of marks for student{i+1}"))
+    list.append(x)
+sum=0
+for i in range(students_numbers):
+    for j in range(list[i]):
+        mark=int(input(f"Enter mark{j+1}for student {i+1}"))
+        grads.append(mark)
+        sum+=mark
+    avarge.append(sum/list[i])
+    maximum.append(max(grads))
+    minimum.append(min(grads))
+    grads.clear()
+for i in range(students_numbers):
+    print(f"Student{i+1}\n Avarge={avarge[i]}\n Min={minimum[i]}\nMax={maximum[i]}")
+
+
+
+
+
+
