545754cbea03772545d0ce871fa576bfc48e3371
GSG-proj <mariamadnan879@gmail.com>
2023-08-20 12:49:10 +0300

    Task 9

---
+++ Task 9.py
@@ -0,0 +1,13 @@
+file=open("ITF8.txt","a")
+name=input("Enter your name")
+age=input("Enter your age")
+DOB=input("Enter your Date of Birth")
+file.write(name)
+file.write(age)
+file.write(DOB)
+
+
+
+
+
+
