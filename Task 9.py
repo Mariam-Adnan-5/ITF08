39d1ea0d94abfc8290118f4b333a151fdd827017
GSG-proj <mariamadnan879@gmail.com>
2023-08-21 11:05:19 +0300

    Task 9

--- Task 9.py
+++ Task 9.py
@@ -2,9 +2,9 @@ with open("ITF8.txt","a") as file:
  name=input("Enter your name")
  age=input("Enter your age")
  DOB=input("Enter your Date of Birth")
- file.write(name)
- file.write(age)
- file.write(DOB)
+ file.write(name+"\t")
+ file.write(age+"\t")
+ file.write(DOB+"\n")
  file.close()
 
 
